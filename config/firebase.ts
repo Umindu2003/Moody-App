79bf42af8a5ee644ce952d2abdc4dd865317189a
Umindu2003 <u4656mee@gmail.com>
2025-12-09 12:46:47 +0530

    created .env and others

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIcsdnErEBMw4ufozdHFomNiWLaLzeeOY",
-  authDomain: "moody-5044f.firebaseapp.com",
-  projectId: "moody-5044f",
-  storageBucket: "moody-5044f.firebasestorage.app",
-  messagingSenderId: "28166919300",
-  appId: "1:28166919300:web:2a4f7de71af3a2d6b29ed1",
-  measurementId: "G-P22YF1MG9T"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
